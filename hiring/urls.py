76742b79f606d4ca325dffc63072d2b45f405db2
Andrew <andrewkok80@gmail.com>
2023-07-27 11:16:53 +0700

    add dump.sql

--- hiring/urls.py
+++ hiring/urls.py
@@ -4,12 +4,12 @@ from .views import (
     RegistrationFormUploadView,
     RegistrationFormGetUUID,
     RegistrationFormGetDecision,
-    RegistrationFormExport
+    # RegistrationFormExport
 )
 
 urlpatterns = [
     path('upload/', RegistrationFormUploadView.as_view(), name="form-upload"),
     path("uuid/", RegistrationFormGetUUID.as_view(), name='get-uuid'),
     path("decision/<uuid:id>", RegistrationFormGetDecision.as_view(), name='get-decision'),
-    path("export", RegistrationFormExport.as_view(), name='export'),
+    # path("export", RegistrationFormExport.as_view(), name='export'),
 ]
